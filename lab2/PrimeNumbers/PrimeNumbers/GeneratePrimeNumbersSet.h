49be787615a4d687344e0ca326884bf67b716951
MariaSneg <mashakotik2090@gmail.com>
2024-03-25 23:06:28 +0300

    Prime numbers

---
+++ lab2/PrimeNumbers/PrimeNumbers/GeneratePrimeNumbersSet.h
@@ -0,0 +1,7 @@
+#pragma once
+#include<set>
+#include <vector>
+
+std::set<int> GeneratePrimeNumberSet(int const& upperBound);
+std::set<int> CreateNumbersSet(int const& upperBound);
+std::vector<bool> Sieve(int const& upperBound);
---
+++ lab2/PrimeNumbers/PrimeNumbers/PrimeNumbers.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <chrono>
+#include "GeneratePrimeNumberSet.h"
+
+int main()
+{
+    int upperBound;
+    std::cin >> upperBound;
+
+    auto startTime = std::chrono::steady_clock::now();
+
+    std::set<int> primeNumbers = GeneratePrimeNumberSet(upperBound);
+
+    for (int i : primeNumbers)
+    {
+        std::cout << i << " ";
+    }
+
+    std::cout << std::endl;
+
+    auto endTime = std::chrono::steady_clock::now();
+
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count() << "ms" << std::endl;
+}
